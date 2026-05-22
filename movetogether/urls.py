a599b86dc432220670324fecb02fb0e394d8c13d
Luis Bretas <luisbretas23@gmail.com>
2026-05-21 21:43:21 -0300

    Adiciona tela de login

--- movetogether/urls.py
+++ movetogether/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', home),
+
+    path('login/', login),
 ]
