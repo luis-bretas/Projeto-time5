afb90ad4497ca6041ab4b0aca5150fedc8dde12d
Guilhermecmv <guigacmv@gmail.com>
2026-06-12 14:02:27 +0000

    login

--- movetogether/urls.py
+++ movetogether/urls.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import home, login, dashboard, grupo, perfil, ranking
+from django.urls.conf import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', home),
-
     path('login/', login),
-   
     path('dashboard/', dashboard),
-    
     path('grupo/', grupo),
-   
     path('perfil/', perfil),
-   
     path('ranking/', ranking),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
